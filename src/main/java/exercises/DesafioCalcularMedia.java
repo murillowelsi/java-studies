6f3dbcb2c9d4f619835edf254838ec375bfc8c41
Murillo Welsi <murillo.welsi@gmail.com>
2021-08-19 09:35:11 +0100

    Added array exercise

---
+++ src/main/java/exercises/DesafioCalcularMedia.java
@@ -0,0 +1,32 @@
+package exercises;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class DesafioCalcularMedia {
+    public static void main(String[] args) {
+        System.out.println("Digite a quantidade de notas que deseja informar.");
+        Scanner read = new Scanner(System.in);
+
+        int tamArr = read.nextInt();
+
+        double []notas = new double[tamArr];
+
+        for (int i = 0; i < tamArr; i++) {
+
+            System.out.println("Digite a nota " + (i + 1) + ":");
+            notas[i] = read.nextDouble();
+
+        }
+
+        double sum = 0;
+
+        for (double nota: notas) {
+            sum += nota;
+        }
+
+        double media = sum/tamArr;
+
+        System.out.println("A média final é: " + media);
+    }
+}
---
+++ src/main/java/exercises/ForEach.java
@@ -0,0 +1,11 @@
+package exercises;
+
+public class ForEach {
+    public static void main(String[] args) {
+        double[] notas = { 5.0, 7.7, 4.5, 4.9 };
+
+        for (double nota : notas) {
+            System.out.println(nota);
+        }
+    }
+}
